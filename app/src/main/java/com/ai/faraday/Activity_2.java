fbc8a9f145269f5cff8db430a4865429678d9630
nik-1207 <nikhil.mohan_cs18@gla.ac.in>
2020-04-02 07:41:59 +0530

    Internet Access
    Signed-off-by: nik-1207 <nikhil.mohan_cs18@gla.ac.in>

--- app/src/main/java/com/ai/faraday/Activity_2.java
+++ app/src/main/java/com/ai/faraday/Activity_2.java
@@ -1,9 +1,11 @@
 package com.ai.faraday;
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.Manifest;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Activity_2 extends AppCompatActivity
@@ -21,27 +23,11 @@ public class Activity_2 extends AppCompatActivity
         voiceButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
-<<<<<<<<< Temporary merge branch 1
             public void onClick(View v)
             {
                 if(!permission.hasPermissions(Activity_2.this, Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.RECORD_AUDIO))
                 {
-                  permission.grantPermission(Activity_2.this, Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.RECORD_AUDIO);
-=========
-            public void onClick(View v) {
-                if (!permission.hasPermissions(Activity_2.this, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO)) {
                     permission.grantPermission(Activity_2.this, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO);
-                }
-                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
-
-
-                try {
-                    startActivityForResult(intent,permission.getGRANT_CODE());
-                } catch (ActivityNotFoundException a) {
-                    Toast.makeText(getApplicationContext(),
-                            getString(R.string.speech_not_supported),Toast.LENGTH_SHORT).show();
->>>>>>>>> Temporary merge branch 2
                 }
             }
         });
